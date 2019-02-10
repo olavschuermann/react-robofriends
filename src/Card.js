a4c132422ec41549b89bcfce482927cb08260155
Olav Schuermann <developer@netzpluslogik.de>
2019-02-10 12:30:49 +0100

    Card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+    return(
+        <div>
+            <img src='' alt='Alt text'/>
+            <div>
+                <h2>Karl Klotz</h2>
+                <p>email@irgendwas.de</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
