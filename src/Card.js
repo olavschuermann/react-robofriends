3f28feca078e897922bb73fc13571b303754079e
Olav Schuermann <developer@netzpluslogik.de>
2019-02-11 17:36:34 +0100

    css grid + styling

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = ({id, name, email}) => {
     return (
         <div>
             <img src={`https://robohash.org/robot-${id}?200x200`} alt='Alt text'/>
-            <div>
+            <div className="robotDetails">
                 <h2>{name}</h2>
                 <p>{email}</p>
             </div>
--- src/robots.js
+++ src/robots.js
@@ -16,7 +16,25 @@ export const robots = [
         name: 'Xaver Quietsch',
         username: 'Xaver',
         email: 'derxaver@email.de'
-    }        
+    },
+    {
+        id: 4,
+        name: 'Marvin 42',
+        username: 'Marvin',
+        email: 'marvin42@email.de'
+    },
+    {
+        id: 5,
+        name: 'Doedeldi Doedelda',
+        username: 'DDoedel',
+        email: 'dd@email.de'
+    },  
+    {
+        id: 6,
+        name: 'Super Duper',
+        username: 'Super',
+        email: 'superduper@email.de'
+    }          
 ]
 
 export default robots;
