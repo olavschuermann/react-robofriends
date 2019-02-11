bf514900b2f8c34718ae43ea24306f66c121756c
Olav Schuermann <developer@netzpluslogik.de>
2019-02-11 18:02:01 +0100

    robofriends app with css animation

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({id, name, email}) => {
 
     return (
-        <div>
+        <div className="robotDiv">
             <img src={`https://robohash.org/robot-${id}?200x200`} alt='Alt text'/>
             <div className="robotDetails">
                 <h2>{name}</h2>
