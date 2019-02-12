f500a2300973e2b707176059bd53f3b47f255785
Olav Schuermann <developer@netzpluslogik.de>
2019-02-12 17:52:55 +0100

    adding header and searchbox

---
+++ src/SearchBox.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchBox = () => {
+    return (
+        <div className='searchDiv'>
+            <input className='searchInput' type='search' placeholder='Roboter suchen' /> 
+        </div>
+    );
+}
+
+export default SearchBox;
