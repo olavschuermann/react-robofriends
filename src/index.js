d2026938c15ca6e073a06027590af24a8464d7c4
Olav Schuermann <developer@netzpluslogik.de>
2019-02-12 17:33:16 +0100

    Adding App component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import Card from './Card';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { robots } from './robots';
-import CardList from './CardList';
 
-ReactDOM.render(
-               <CardList robots={robots}/>
-                , document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
