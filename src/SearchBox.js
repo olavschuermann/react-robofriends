3aeeb457b3190b000873cddc3ae83379948fa99d
Olav Schuermann <developer@netzpluslogik.de>
2019-02-12 18:25:47 +0100

    adding searchbox filter

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const SearchBox = () => {
+const SearchBox = ( {searchfield, searchChange} ) => {
     return (
         <div className='searchDiv'>
-            <input className='searchInput' type='search' placeholder='Roboter suchen' /> 
+            <input className='searchInput' type='search' placeholder='Roboter suchen' 
+            onChange={searchChange}/> 
         </div>
     );
 }
