5611b6d9e4e6e2e34723a06d3a2e61c9e7f24a05
Olav Schuermann <developer@netzpluslogik.de>
2019-02-16 17:10:42 +0100

    adding directories

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import Scroll from "./Scroll";
-import { robots } from './robots';
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+import { robots } from '../robots';
 
 
 class App extends Component {
